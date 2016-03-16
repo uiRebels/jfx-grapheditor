fe5627fdf2a5e85450b140258437958e35bd0de9
bnamestka <robert.namestka@gmail.com>
2016-03-16 16:13:06 -0400

    Initial code stubs for MVVM implementation

---
+++ src/main/java/org/uirebels/grapheditor/experimental/model/JFXGraphEditorModel.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.uirebels.grapheditor.experimental.model;
+
+import org.uirebels.grapheditor.model.*;
+
+/**
+ *
+ * @author bnamestka
+ */
+public class JFXGraphEditorModel {
+    
+}
